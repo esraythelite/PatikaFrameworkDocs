3c0155e9dbbd4c8a0288c38e534614ed0a9b15f3
saimnasirpatika <saim.nasir@patikaglobal.com>
2022-11-25 12:56:46 +0300

    jwt docs

---
+++ src/components/Framework/JwtToken/JwtTokenInterfaces.js
@@ -0,0 +1,33 @@
+import React from 'react' 
+import DocPaper from '../../DocPaper';
+const contents = [
+  {
+    order: 1,
+    type: 'code',
+    title: 'ITokenGenerator',
+    language: 'csharp',
+    startingLineNumber: 3,
+    item: `
+    namespace Patika.Framework.Identity.JwtToken.Interfaces
+    {
+        public interface ITokenGenerator
+        {
+            Token GenerateToken(IEnumerable<Claim> claims);
+            Token RefreshToken(string accessToken);
+            ClaimsPrincipal? GetPrincipalFromExpiredToken(string token);
+        }
+    }`,
+    descriptions: [
+      "Token generator helps you to generate token.",  
+      "You can generate hasshed token and refresh token by configuration",  
+    ],
+  } 
+]
+
+const header = 'Patika.Framework.Identity.JwtToken.Interfaces';
+const JwtTokenInterfaces = () => {
+  return (
+    <DocPaper  header={header} contents={contents} />     
+  )
+}  
+export default JwtTokenInterfaces
---
+++ src/components/Framework/JwtToken/JwtTokenServices.js
@@ -0,0 +1,41 @@
+import { Box, Card, CardContent, CardHeader, Paper, Stack, Typography } from '@mui/material'
+import { grey, purple } from '@mui/material/colors'
+import React from 'react'
+import Tab from '@mui/material/Tab';
+import { TabContext, TabList, TabPanel } from '@mui/lab' 
+import TokenGenerator from './TokenGenerator';
+
+const JwtTokenServices = () => {
+
+  const [selectedTab, setSelectedTab] = React.useState('TokenGenerator');
+  const handleChangeTab = (event, newValue) => {
+    setSelectedTab(newValue);
+  };
+
+  const width = '95%';
+  return (
+    <Box
+      sx={{ width: '100%',  typography: 'body1' }}
+    >
+      <TabContext value={selectedTab}  >
+        <Box sx={{ position: "fixed", bgcolor: grey[50] , border: 2,   borderColor: 'divider'  }}>
+          <TabList
+            onChange={handleChangeTab}
+            //orientation="vertical" 
+              indicatorColor="primary"
+            sx={{ alignContent: 'center', overflow: 'visible' }}
+            selectionFollowsFocus
+          >
+            <Tab label='TokenGenerator' value='TokenGenerator' sx={{ alignContent: 'center' }} />
+          </TabList>
+
+        </Box>
+        <TabPanel value="TokenGenerator" sx={{ width: width }}>
+          <TokenGenerator />
+        </TabPanel> 
+        
+      </TabContext>
+    </Box>
+  )
+}
+export default JwtTokenServices
